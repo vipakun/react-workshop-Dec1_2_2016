4c819671230e5e7c07cc5d205c016292d8d323ac
Xiangkun Yang <vipakun@hotmail.com>
2016-12-02 11:00:54 -0600

    search state and filter

--- js/Search.js
+++ js/Search.js
@@ -3,24 +3,34 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
-  getInitialState() {
+  getInitialState () {
     return {
-      searchTerm: 'this is default string'
+      searchTerm: ''
     }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
   },
   render () {
     return (
       <div className='search'>
         <header>
           <h1>svideo</h1>
-          <input type='text' placeholder='Search' value={this.state.searchTerm}/>
+          <input type='text' placeholder='Search'
+            value={this.state.searchTerm} onChange={this.handleSearchTermChange} />
         </header>
         <div>
-          {preload.shows.map((show) => {
-            return (
-              <ShowCard key={show.imdbID} {...show} />
+          {preload.shows
+            .filter((show) => {
+              return `${show.title} ${show.description}`
+                .toUpperCase().indexOf(this.state.searchTerm
+                .toUpperCase()) >= 0
+            })
+            .map((show) => {
+              return (
+                <ShowCard key={show.imdbID} {...show} />
             )
-          })}
+            })}
         </div>
       </div>
     )
