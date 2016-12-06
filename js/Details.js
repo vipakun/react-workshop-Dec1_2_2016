e5b6e0ef9518eb0ba72e7c4ff872b63ce0a1ffa9
Xiangkun Yang <vipakun@hotmail.com>
2016-12-06 14:39:30 -0600

    add redux

--- js/Details.js
+++ js/Details.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Header from './Header'
 
 const { shape, string } = React.PropTypes
@@ -10,17 +11,37 @@ const Details = React.createClass({
       year: string,
       poster: string,
       trailer: string,
-      description: string
+      description: string,
+      imdbID: string
     })
+  },
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://wwww.omdbapi.com/?i=${this.props.show.imdbID}`)
+      .then((response) => {
+        this.setState({omdbData: response.data})
+      })
+      .catch((error) => console.error('axios error', error))
   },
   render () {
     const { title, year, poster, trailer, description } = this.props.show
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3>{this.state.omdbData.imdbRating}</h3>
+    } else {
+      rating = <img src='/public/img/loading.png' alt='loading indicator' />
+    }
     return (
       <div className='details'>
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {rating}
           <img src={`/public/img/posters/${poster}`} />
           <p>{description}</p>
         </section>
--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import { setSearchTerm } from './actionCreators'
+const { string, func, object } = React.PropTypes
 
 const Landing = React.createClass({
+  contextTypes: {
+    router: object
+  },
+  propTypes: {
+    searchTerm: string,
+    dispatch: func
+  },
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  },
+  handleSearchSubmit (event) {
+    event.preventDefault()
+    this.context.router.transitionTo('/search')
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='search' />
+        <form onSubmit={this.handleSearchSubmit}>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.props.searchTerm}
+            type='text'
+            placeholder='search'
+          />
+        </form>
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Landing)
