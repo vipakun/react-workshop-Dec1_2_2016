a41783d9d7604d502beb2d699bb1c0f9fe235d0b
Xiangkun Yang <vipakun@hotmail.com>
2016-12-08 13:03:08 -0600

    code splitting Part 2
    Details and Search

--- js/App.js
+++ js/App.js
@@ -3,8 +3,6 @@ import { Match } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './store'
 import AsyncRoute from './AsyncRoute'
-import Search from './Search'
-import Details from './Details'
 import preload from '../public/data.json'
 if (global) {
   global.System = { import () {} }
@@ -21,13 +19,14 @@ const App = () => {
         />
         <Match
           pattern='/search'
-          component={(props) => <Search shows={preload.shows} {...props} />}
+          component={(props) => <AsyncRoute props={Object.assign({shows: preload.shows}, props)} loadingPromise={System.import('./Search')} />}
         />
         <Match
+          exactly
           pattern='/details/:id'
           component={(props) => {
             const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
-            return <Details show={shows[0]} {...props} />
+            return <AsyncRoute props={Object.assign({show: shows[0]}, props)} loadingPromise={System.import('./Details')} />
           }}
         />
       </div>
